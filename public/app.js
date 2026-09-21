157e1ea38607ad6f72a9eca6515cdc56bd339a6d
severine-helo-collab <kouettes@gmail.com>
2026-09-21 21:53:08 +0200

    Update Supabase URL in app.js

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 // ==========================================
 // CONFIGURATION SUPABASE
 // ==========================================
-const SUPABASE_URL = 'https://votredomaine.supabase.co';
+const SUPABASE_URL = 'https://iyxurkbceiirjdigcyak.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5eHVya2JjZWlpcmpkaWdjeWFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwNDYzODQsImV4cCI6MjEwNDYyMjM4NH0.MGBADlkxP307mbUvU_07OEhN5sfqv9_wSTqIP5AVK-s';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
